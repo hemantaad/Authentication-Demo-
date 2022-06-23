94f6f9a5fa13d96b0bcc759a54c558e8aab40741
Aditya Adhikari <hemantaad@gmail.com>
2022-06-23 13:07:34 +0545

    User Login

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from user.views import UserRegistrationView
+from user.views import UserRegistrationView, UserLoginView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
 
- ]
+]
--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
 from rest_framework import status
+from django.contrib.auth import authenticate
 
 
 class UserRegistrationView(APIView):
@@ -12,4 +13,18 @@ class UserRegistrationView(APIView):
             user = serializer.save()
             return Response({'msg': 'Registration Success'}, status=status.HTTP_201_CREATED)
 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                return Response({'msg': 'Login Success'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors': {'non_field_errors':['Email or Password is not valid.']}}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
